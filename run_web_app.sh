555d8e5827a7753f572c473453695b536227cb4f
kerstenbreuer <kersten-breuer@outlook.com>
2019-06-12 20:17:06 +0200

    automatic init/migration/upgrate of database

--- run_web_app.sh
+++ run_web_app.sh
@@ -4,8 +4,8 @@ export FLASK_DEBUG=1
 export FLASK_APP=web_app_exec.py
 
 # only once:
-# flask db init
-# flask db migrate -m "init"
-# flask db upgrade
+flask db init
+flask db migrate -m "init"
+flask db upgrade
 
 flask run
