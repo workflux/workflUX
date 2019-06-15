26655ff0ba8c85b718157efef5560f30b4248ccf
kerstenbreuer <kersten-breuer@outlook.com>
2019-06-15 20:37:30 +0200

    bg script talk to database

--- cwlab/exec/cwlab_bg_exec.py
+++ cwlab/exec/cwlab_bg_exec.py
@@ -1,16 +1,38 @@
 import sys
 import os
 import subprocess
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.automap import automap_base
 
+# commandline arguments
 exec_dir = sys.argv[1]
 cwl_dir = sys.argv[2]
 db_uri = sys.argv[3]
 exec_db_id = sys.argv[4]
 
+# open connection to database
+engine = create_engine(db_uri)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Exec = Base.classes.exec
+
+# retrieve infos from database
+exec_db_entry = session.query(Exec).get(int(exec_db_id))
+job_id = exec_db_entry.job_id
+run_id = exec_db_entry.run_id
+cwl = exec_db_entry.cwl
+exec_profile = exec_db_entry.exec_profile
+
+# construct paths:
 subprocess.call(
     "echo " + exec_dir + " " +
     cwl_dir + " " +
     db_uri + " " +
-    exec_db_id + " " +
-    " >> /mnt/c/Users/kerst/OneDrive/home/CWLab/test", shell=True
+    job_id + " " +
+    run_id + " " +
+    cwl + " " +
+    ">> /mnt/c/Users/kerst/OneDrive/home/CWLab/test", shell=True
 )
---
+++ scratch/test_files/test_cwl_bg_exec.sh
@@ -0,0 +1,5 @@
+python3 cwlab/exec/cwlab_bg_exec.py \
+    /mnt/c/Users/kerst/OneDrive/home/CWLab/scratch/exec \
+    /mnt/c/Users/kerst/OneDrive/home/CWLab/scratch/CWL \
+    sqlite:////mnt/c/Users/kerst/OneDrive/home/CWLab/scratch/database/cwlab.db \
+    8
