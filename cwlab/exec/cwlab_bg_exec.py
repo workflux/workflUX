439568178a105f0a3440bb3d4ed04d7f54d7dc7d
kerstenbreuer <kersten-breuer@outlook.com>
2019-06-15 17:43:52 +0200

    test background execution

--- cwlab/exec/cwlab_bg_exec.py
+++ cwlab/exec/cwlab_bg_exec.py
@@ -1,3 +1,16 @@
+import sys
+import os
 import subprocess
 
-subprocess.call("sleep 10 && touch test", shell=True)
+exec_dir = sys.argv[1]
+cwl_dir = sys.argv[2]
+db_uri = sys.argv[3]
+exec_db_id = sys.argv[4]
+
+subprocess.call(
+    "echo " + exec_dir + " " +
+    cwl_dir + " " +
+    db_uri + " " +
+    exec_db_id + " " +
+    " >> /mnt/c/Users/kerst/OneDrive/home/CWLab/test", shell=True
+)
