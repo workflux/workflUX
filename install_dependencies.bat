32eb61f19e81d4e78621e2f41626fe445f21de0b
kerstenbreuer <kersten-breuer@outlook.com>
2019-06-23 01:31:48 +0200

    fix

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,7 +1,9 @@
-py -m pip install flask flask_wtf flask-sqlalchemy flask-migrate flask-login flask-sqlalchemy
+pip install flask flask_wtf flask-sqlalchemy flask-migrate flask-login flask-sqlalchemy
 
-py -m pip install pyexcel pyexcel-io pyexcel-ods pyexcel-ods3 pyexcel-xls pyexcel-xlsx
+pip install pyexcel pyexcel-io pyexcel-ods pyexcel-ods3 pyexcel-xls pyexcel-xlsx
 
-py -m pip install PyYAML unidecode
+pip install PyYAML unidecode
 
-py -m pip install cwltool
+pip install cwltool
+
+pip install pyfladesk
