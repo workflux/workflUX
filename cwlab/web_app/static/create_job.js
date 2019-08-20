5a5a9abd4b422cf3d427e95b5f3d99eefb2c87f5
kerstenbreuer <kersten-breuer@outlook.com>
2019-08-20 12:02:50 +0200

    seperated JobParamFormSpreadsheet component

--- cwlab/web_app/static/create_job.js
+++ cwlab/web_app/static/create_job.js
@@ -1,80 +1,25 @@
-
-class JobCreationPrep extends React.Component {
-    // Essential information are reqeusted by the user in order to
-    // build a job template form. 
-    // Following informations are needed:
-    //  - which parameter shall be global and which run-specific
-    //  - how many jobs (/samples) shall be created
-    constructor(props) {
+class JobParamFormSpreadsheet extends React.Component {
+    constructor(props){
         super(props);
-        // Inputs:
-        // props.configData
         // props.cwlTarget
+        // props.param_modes
+        // props.run_mode
+        // props.run_names
+        // props.jobId
 
-        let paramModes={} // container for param mode (is_run_specific true/false)
-        this.props.configData.params.map((p) =>
-            paramModes[p.param_name] = p.is_run_specific
-        )
         this.state = {
-            run_mode: false, 
-            run_names: ["job1", "job2", "job3"],
-            param_modes: paramModes,
-            job_name: "new_job",
             sheet_format: "xlsx",
-            file_transfer_status: "none", // if not none, all input will be disabled
+            file_transfer_status: "none",
             job_creation_status: "none",
             form_passed_validation: false,
             sheetFormMessages: [],
-            jobCreationMessages: [],
+            jobCreationMessages: []
         }
 
-        // construct job_id:
-        const date = new Date()
-        let year = date.getFullYear().toString()
-        let month = date.getMonth()
-        month = (month < 10) ? ("0" + month.toString()) : (month.toString())
-        let day = date.getDate()
-        day = (day < 10) ? ("0" + day.toString()) : (day.toString())
-        const dateString = year + month + day
-        let randomNumber = Math.round(Math.random()*1000)
-        randomNumber = (randomNumber < 100) ? ("0" + randomNumber.toString()) : (randomNumber.toString())
-        this.jobIdNum = dateString + "_" + randomNumber
-        
-
-        this.changeJobName = this.changeJobName.bind(this);
-        this.changeParamMode = this.changeParamMode.bind(this);
-        this.changeRunMode = this.changeRunMode.bind(this);
-        this.changeRunNames = this.changeRunNames.bind(this);
         this.changeSheetFormat = this.changeSheetFormat.bind(this);
         this.genFormSheet = this.genFormSheet.bind(this);
         this.handleFormSheetUpload = this.handleFormSheetUpload.bind(this)
-        this.createJob = this.createJob.bind(this)
         this.ajaxRequest = ajaxRequest.bind(this)
-    }
-
-    changeJobName(event){
-        let jobNameString = event.currentTarget.value.trim().replaceAll(" ", "_")
-        this.setState({"job_name": jobNameString})
-    }
-
-    changeParamMode(param_name, is_run_specific){
-        let update = {}
-        update[param_name] = is_run_specific
-        this.setState({
-            param_modes: Object.assign(this.state.param_modes, update)
-        })
-    } 
-
-    changeRunMode(value, new_bool){
-        this.setState({"run_mode": new_bool})
-    }
-
-    changeRunNames(event){
-        let runNameString = event.currentTarget.value
-        runNameString = runNameString.replaceAll("\n", ",").replaceAll("\t", ",").replaceAll(";", ",")
-        let runNames = runNameString.split(",")
-        runNames = runNames.map((n) => n.trim().replace(" ", "_"))
-        this.setState({"run_names": runNames})
     }
 
     changeSheetFormat(event){
@@ -88,10 +33,10 @@ class JobCreationPrep extends React.Component {
             messageVar: "sheetFormMessages",
             sendData: {
                 cwl_target: this.props.cwlTarget,
-                param_modes: this.state.param_modes,
-                run_mode: this.state.run_mode, 
-                run_names: this.state.run_names.filter((r) => r != ""),
-                job_id: (this.jobIdNum + "_" + this.state.job_name),
+                param_modes: this.props.param_modes,
+                run_mode: this.props.run_mode, 
+                run_names: this.props.run_names.filter((r) => r != ""),
+                job_id: this.props.jobId,
                 sheet_format: this.state.sheet_format
             },
             route: routeGenParamFormSheet,
@@ -106,116 +51,9 @@ class JobCreationPrep extends React.Component {
         this.setState({form_passed_validation: isSuccess})
     }
 
-    createJob() {
-        this.ajaxRequest({
-            statusVar: "job_creation_status",
-            statusValueDuringRequest: "in_progress",
-            messageVar: "jobCreationMessages",
-            sendData: {
-                job_id: (this.jobIdNum + "_" + this.state.job_name),
-                sheet_format: this.state.sheet_format //#! problematic: if format selector is changed after sheet was already submitted
-            },
-            route: routeCreateJob
-        })
-    }
-
     render() {
-        const paramTable = (
-            <div style={ {maxHeight:"50vh", overflowY: "auto"} }>
-                <table className="w3-table w3-bordered w3-border">
-                    <thead className="w3-text-green">
-                        <tr>
-                            <th>Parameter</th>
-                            <th>Type</th>
-                            {this.state.run_mode ? (<th>Mode</th>) : (null)}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.props.configData.params.map( (p) => (
-                            <tr key={p.param_name}> 
-                                <td>{p.param_name}</td>
-                                <td>{p.type}</td>
-                                {this.state.run_mode ? (
-                                        <td>
-                                            global &nbsp;
-                                            <BooleanSlider
-                                                name="param_mode_select"
-                                                value={p.param_name}
-                                                onChange={this.changeParamMode}
-                                                checked={this.state.param_modes[p.param_name]}
-                                                disabled={this.state.file_transfer_status != "none"}
-                                            />
-                                            &nbsp; run-specific
-                                        </td>
-                                    ) : (null)
-                                }
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        )
-
-        const jobIdForm = (
-            <div>
-                <p>
-                    <span className="w3-text-green">Job ID:</span>&nbsp;
-                    <IneditableValueField>
-                        {this.jobIdNum + "_" + this.state.job_name }
-                    </IneditableValueField>
-                </p>
-                <p>
-                    <label className="w3-text-green">Job name:</label><br/>
-                    Please enter a job name (no whitespaces allowed).<br/>
-                    <input type="text"
-                        className="w3-input w3-border"
-                        name="job_name"
-                        value={this.state.job_name}
-                        onChange={this.changeJobName}
-                        disabled={this.state.file_transfer_status != "none"}
-                    />
-                </p>
-            </div>
-        )
-
-        const runNumberForm = (
-            <div>
-                <span className="w3-text-green">Runs per job:</span>&nbsp;
-                single run &nbsp;
-                <BooleanSlider
-                    name="create_multi_run_job"
-                    value="create_multi_run_job"
-                    onChange={this.changeRunMode}
-                    checked={this.run_mode}
-                    disabled={this.state.file_transfer_status != "none"}
-                />
-                &nbsp; multiple runs
-                {this.state.run_mode ? (
-                    <span>
-                        <Message type="hint">Please choose parameter modes in the above table.</Message>
-                        <label className="w3-text-green">Run names/IDs:</label><br/>
-                        Please enter unique, comma-seperated IDs.
-                        No whitespaces allowed (if inserted will be automatically converted to "_"). 
-                        Hint: you may copy&paste cells from an excel file.
-                        <textArea className="w3-input w3-border"
-                            rows="2"
-                            name="create_multi_run_job" 
-                            value={this.state.run_names.join(", ").trim()}
-                            onChange={this.changeRunNames}
-                            disabled={this.state.file_transfer_status != "none"}
-                        />
-                    </span>
-                    ) : (null)
-                }
-            </div>
-        )
-
-        const genParamForm = (
+        return(
             <div className="w3-container">
-                {/* <div className="w3-container">
-                    <span className="w3-text-green">As HTML form:</span>
-                </div>
-                <hr></hr> */}
                 <div className="w3-container">
                     <span className="w3-text-green">As spreadsheet form:</span>
                     <ol>
@@ -267,7 +105,179 @@ class JobCreationPrep extends React.Component {
                     <DisplayServerMessages messages={this.state.jobCreationMessages} />
 
                 </div>
-            </div>   
+            </div> 
+        )
+    }
+}
+
+
+class JobCreationPrep extends React.Component {
+    // Essential information are reqeusted by the user in order to
+    // build a job template form. 
+    // Following informations are needed:
+    //  - which parameter shall be global and which run-specific
+    //  - how many jobs (/samples) shall be created
+    constructor(props) {
+        super(props);
+        // Inputs:
+        // props.configData
+        // props.cwlTarget
+
+        let paramModes={} // container for param mode (is_run_specific true/false)
+        this.props.configData.params.map((p) =>
+            paramModes[p.param_name] = p.is_run_specific
+        )
+        this.state = {
+            run_mode: false, 
+            run_names: ["job1", "job2", "job3"],
+            param_modes: paramModes,
+            job_name: "new_job"
+        }
+
+        // construct job_id:
+        const date = new Date()
+        let year = date.getFullYear().toString()
+        let month = date.getMonth()
+        month = (month < 10) ? ("0" + month.toString()) : (month.toString())
+        let day = date.getDate()
+        day = (day < 10) ? ("0" + day.toString()) : (day.toString())
+        const dateString = year + month + day
+        let randomNumber = Math.round(Math.random()*1000)
+        randomNumber = (randomNumber < 100) ? ("0" + randomNumber.toString()) : (randomNumber.toString())
+        this.jobIdNum = dateString + "_" + randomNumber
+        
+
+        this.changeJobName = this.changeJobName.bind(this);
+        this.changeParamMode = this.changeParamMode.bind(this);
+        this.changeRunMode = this.changeRunMode.bind(this);
+        this.changeRunNames = this.changeRunNames.bind(this);
+        // this.createJob = this.createJob.bind(this)
+        this.ajaxRequest = ajaxRequest.bind(this)
+    }
+
+    changeJobName(event){
+        let jobNameString = event.currentTarget.value.trim().replaceAll(" ", "_")
+        this.setState({"job_name": jobNameString})
+    }
+
+    changeParamMode(param_name, is_run_specific){
+        let update = {}
+        update[param_name] = is_run_specific
+        this.setState({
+            param_modes: Object.assign(this.state.param_modes, update)
+        })
+    }
+
+    changeRunMode(value, new_bool){
+        this.setState({"run_mode": new_bool})
+    }
+
+    changeRunNames(event){
+        let runNameString = event.currentTarget.value
+        runNameString = runNameString.replaceAll("\n", ",").replaceAll("\t", ",").replaceAll(";", ",")
+        let runNames = runNameString.split(",")
+        runNames = runNames.map((n) => n.trim().replace(" ", "_"))
+        this.setState({"run_names": runNames})
+    }
+
+    // createJob() {
+    //     this.ajaxRequest({
+    //         statusVar: "job_creation_status",
+    //         statusValueDuringRequest: "in_progress",
+    //         messageVar: "jobCreationMessages",
+    //         sendData: {
+    //             job_id: (this.jobIdNum + "_" + this.state.job_name),
+    //             sheet_format: this.state.sheet_format //#! problematic: if format selector is changed after sheet was already submitted
+    //         },
+    //         route: routeCreateJob
+    //     })
+    // }
+
+    render() {
+        const paramTable = (
+            <div style={ {maxHeight:"50vh", overflowY: "auto"} }>
+                <table className="w3-table w3-bordered w3-border">
+                    <thead className="w3-text-green">
+                        <tr>
+                            <th>Parameter</th>
+                            <th>Type</th>
+                            {this.state.run_mode ? (<th>Mode</th>) : (null)}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.configData.params.map( (p) => (
+                            <tr key={p.param_name}> 
+                                <td>{p.param_name}</td>
+                                <td>{p.type}</td>
+                                {this.state.run_mode ? (
+                                        <td>
+                                            global &nbsp;
+                                            <BooleanSlider
+                                                name="param_mode_select"
+                                                value={p.param_name}
+                                                onChange={this.changeParamMode}
+                                                checked={this.state.param_modes[p.param_name]}
+                                            />
+                                            &nbsp; run-specific
+                                        </td>
+                                    ) : (null)
+                                }
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        )
+
+        const jobIdForm = (
+            <div>
+                <p>
+                    <span className="w3-text-green">Job ID:</span>&nbsp;
+                    <IneditableValueField>
+                        {this.jobIdNum + "_" + this.state.job_name }
+                    </IneditableValueField>
+                </p>
+                <p>
+                    <label className="w3-text-green">Job name:</label><br/>
+                    Please enter a job name (no whitespaces allowed).<br/>
+                    <input type="text"
+                        className="w3-input w3-border"
+                        name="job_name"
+                        value={this.state.job_name}
+                        onChange={this.changeJobName}
+                    />
+                </p>
+            </div>
+        )
+
+        const runNumberForm = (
+            <div>
+                <span className="w3-text-green">Runs per job:</span>&nbsp;
+                single run &nbsp;
+                <BooleanSlider
+                    name="create_multi_run_job"
+                    value="create_multi_run_job"
+                    onChange={this.changeRunMode}
+                    checked={this.run_mode}
+                />
+                &nbsp; multiple runs
+                {this.state.run_mode ? (
+                    <span>
+                        <Message type="hint">Please choose parameter modes in the above table.</Message>
+                        <label className="w3-text-green">Run names/IDs:</label><br/>
+                        Please enter unique, comma-seperated IDs.
+                        No whitespaces allowed (if inserted will be automatically converted to "_"). 
+                        Hint: you may copy&paste cells from an excel file.
+                        <textArea className="w3-input w3-border"
+                            rows="2"
+                            name="create_multi_run_job" 
+                            value={this.state.run_names.join(", ").trim()}
+                            onChange={this.changeRunNames}
+                        />
+                    </span>
+                    ) : (null)
+                }
+            </div>
         )
 
         return(
@@ -282,8 +292,13 @@ class JobCreationPrep extends React.Component {
                 {runNumberForm}
                 <hr/>
                 <h3>Generate Parameter Form:</h3>
-                {genParamForm}
-                <hr/>
+                <JobParamFormSpreadsheet
+                    cwlTarget={this.props.cwlTarget}
+                    param_modes={this.state.param_modes}
+                    run_mode={this.state.run_mode}
+                    run_names={this.state.run_names}
+                    jobId={this.jobIdNum + "_" + this.state.job_name}
+                />
             </div>
         )
     }
