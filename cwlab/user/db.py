310765544f279e0d1793ffd0f9fafae3a33d9e57
kerstenbreuer <kersten-breuer@outlook.com>
2019-09-11 17:12:32 +0200

    use flask login

--- cwlab/user/db.py
+++ cwlab/user/db.py
@@ -1,6 +1,8 @@
 from cwlab import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
