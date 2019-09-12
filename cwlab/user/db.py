757240b621c398772362b4817ceb0bf948f0c784
kerstenbreuer <kersten-breuer@outlook.com>
2019-09-12 13:49:55 +0200

    added list of allowed levels

--- cwlab/user/db.py
+++ cwlab/user/db.py
@@ -2,6 +2,8 @@ from cwlab import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+allowed_levels = ["admin", "user"]
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
