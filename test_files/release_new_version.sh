c4e1728aa36b048e5b4a64c775bdb4760e81a503
kerstenbreuer <kersten-breuer@outlook.com>
2019-10-17 14:01:14 +0200

    add utility script for releasing new versions

---
+++ test_files/release_new_version.sh
@@ -0,0 +1,17 @@
+# update version in setup and init
+
+# convert Readme.md to Readme.rst
+pandoc -f markdown -t rst -o Readme.rst Readme.md
+
+# make dist:
+rm -rf dist/*
+python setup.py sdist
+
+# check dist
+twine check dist/*
+
+# upload to test pypi
+twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
+# upload to production pypi
+twine upload dist/*
