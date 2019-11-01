6f77b4c73a8df3642929d3fe9b13f8674cd12c1c
kerstenbreuer <kersten-breuer@outlook.com>
2019-11-01 23:16:34 +0100

    update import

--- cwlab/web_app/static/import_cwl.js
+++ cwlab/web_app/static/import_cwl.js
@@ -3,8 +3,15 @@ class ImportCWLRoot extends React.Component {
     render() {
         return(
             <div className="w3-panel">
+                <span className="w3-text-green">Choose a name:</span>&nbsp;
+                <input type="text"
+                    className="w3-input w3-border"
+                    name="job_name"
+                    value={this.state.job_name}
+                    onChange={this.changeJobName}
+                />
                 <FileUploadComponent
-                    requestRoute={routeCwlImport}
+                    requestRoute={routeImportPackedCwl}
                     instruction="Please select a CWL file:"
                 />
             </div>
