abc963191e470bfb852b1354a4fc2e37d49bdf49
kerstenbreuer <kersten-breuer@outlook.com>
2019-11-14 20:07:28 +0100

    fix month

--- cwlab/web_app/static/create_job.js
+++ cwlab/web_app/static/create_job.js
@@ -1373,7 +1373,7 @@ class JobCreationPrep extends React.Component {
         // construct job_id:
         const date = new Date()
         let year = date.getFullYear().toString()
-        let month = date.getMonth()
+        let month = date.getMonth() + 1
         month = (month < 10) ? ("0" + month.toString()) : (month.toString())
         let day = date.getDate()
         day = (day < 10) ? ("0" + day.toString()) : (day.toString())
