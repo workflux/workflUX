3c02440fa5acaed5c7413f6b9144e1b655bdc190
kerstenbreuer <kersten-breuer@outlook.com>
2019-11-19 16:35:26 +0100

    get workflow type from file ext

--- cwlab/wf_input/read_wf.py
+++ cwlab/wf_input/read_wf.py
@@ -1,13 +1,19 @@
 from .read_cwl import read_config_from_cwl_file
 from .read_janis import read_config_from_janis_file
 
+supported_workflow_exts = {
+    "CWL": ["CWL", "cwl"],
+    "janis": [".py"]
+}
+
 def read_config_from_workflow(workflow_file, wf_type):
     if wf_type is None:
         # get workflow type from file extention:
         ext = os.path.splitext(workflow_file)[1]
-        if ext in ["CWL", "cwl"]:
+
+        if ext in supported_workflow_exts["CWL"]:
             wf_type = "CWL"
-        elif ext in ["py"]:
+        elif ext in supported_workflow_exts["janis"]:
             wf_type = "janis"
         else:
             raise AssertionError("Could not determine workflow type from file name, please specify the workflow type manually.")
@@ -16,5 +22,10 @@ def read_config_from_workflow(workflow_file, wf_type):
     elif wf_type == "janis":
         configs, metadata = read_config_from_janis_file(workflow_file)
     else:
-        raise AssertionError("Unkown workflow type: {}".format(wf_type))
+        raise AssertionError(
+            "Unkown workflow type \"{}\" specified, only following types are supported: ".format(
+                wf_type,
+                ", ".join(supported_workflow_exts.keys())
+            )
+        )
     return configs, metadata
