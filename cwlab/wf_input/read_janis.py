fa5b040cb5db8e4b751870fca34befc5882e528d
kerstenbreuer <kersten-breuer@outlook.com>
2019-11-20 14:19:50 +0100

    fix naming of janis_file

--- cwlab/wf_input/read_janis.py
+++ cwlab/wf_input/read_janis.py
@@ -93,8 +93,8 @@ def read_config_from_janis_file(janis_file):
         "doc": workflow.metadata.documentation \
             if workflow.metadata.documentation is not None else "",
         "workflow_type": "janis",
-        "workflow_name": os.path.basename(cwl_file),
-        "workflow_path": os.path.abspath(cwl_file),
+        "workflow_name": os.path.basename(janis_file),
+        "workflow_path": os.path.abspath(janis_file),
     }
     inp_records = workflow.inputs()
     for inp_rec in inp_records:
