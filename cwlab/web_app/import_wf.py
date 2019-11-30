27625da6c139496360c916b755010c4dd14f845f
kerstenbreuer <kersten-breuer@outlook.com>
2019-11-30 09:12:28 +0100

    explicitly set wf_type

--- cwlab/web_app/import_wf.py
+++ cwlab/web_app/import_wf.py
@@ -28,6 +28,7 @@ def upload_wf():
         # save the file to the CWL directory:
         metadata = json_loads(request.form.get("meta"))
         import_filename = secure_filename(import_file.filename) 
+        wf_type = data_req["wf_type"] if "wf_type" in data_req.keys() else None
         
         temp_dir = make_temp_dir()
         imported_filepath = os.path.join(temp_dir, import_filename)
@@ -36,7 +37,7 @@ def upload_wf():
         import_name = secure_filename(metadata["import_name"]) \
             if "import_name" in metadata.keys() and metadata["import_name"] != "" \
             else import_filename
-        import_wf_(wf_path=imported_filepath, name=import_name)
+        import_wf_(wf_path=wf_path, name=import_name, wf_type=wf_type)
         
         try:
             rmtree(temp_dir)
@@ -148,8 +149,9 @@ def import_wf_by_path_or_url():
         login_required()
         data_req = request.get_json()
         wf_path = data_req["wf_path"]
-        is_url = data_req["is_url"] if "is_url" in data_req.keys() else False
+        is_url = data_req["is_url"] if "is_url" in data_req.keys() else None
         import_name = data_req["import_name"]
+        wf_type = data_req["wf_type"] if "wf_type" in data_req.keys() else None
 
         if is_url:
             vaidate_url(wf_path)
@@ -163,7 +165,7 @@ def import_wf_by_path_or_url():
                 check_if_path_in_dirs(wf_path, allowed_dirs) is not None, \
                 "Path does not exist or you have no permission to enter it."
 
-        import_wf_(wf_path=wf_path, name=import_name)
+        import_wf_(wf_path=wf_path, name=import_name, wf_type=wf_type)
 
         messages.append( { 
             "time": get_time_string(),
