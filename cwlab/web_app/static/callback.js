a6220f4cfc8b9beede87e8ebf408dddcb4313db8
Marius Dieckmann <mariusdieckmann90@gmail.com>
2020-04-17 11:56:16 +0200

    added error handling in signinRedirectCallback handler

--- cwlab/web_app/static/callback.js
+++ cwlab/web_app/static/callback.js
@@ -1,6 +1,11 @@
 window.onload = function () {
     new Oidc.UserManager().signinRedirectCallback().then(function() {
         window.location = "/"
-    })
+    }).catch(function (e) {
+        console.log(e);
+        document.getElementById('error_explanation').innerText = "Problem has occured";
+        document.getElementById('error_place').innerText = e;
+    });
+
     
 }
