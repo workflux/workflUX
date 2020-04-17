d7ebed0aa763a2e8587dc6d7480b3d1471545209
KerstenBreuer <kersten-breuer@outlook.com>
2020-04-17 14:51:41 +0200

    added responce mode to callback

--- cwlab/web_app/static/callback.js
+++ cwlab/web_app/static/callback.js
@@ -1,11 +1,9 @@
 window.onload = function () {
-    new Oidc.UserManager().signinRedirectCallback().then(function() {
+    new Oidc.UserManager({ response_mode: "query"}).signinRedirectCallback().then(function() {
         window.location = "/"
     }).catch(function (e) {
         console.log(e);
         document.getElementById('error_explanation').innerText = "Problem has occured";
         document.getElementById('error_place').innerText = e;
-    });
-
-    
+    });   
 }
