cefdf9612496d43a7206ba74500aa06b8186bc36
Kersten Breuer <kersten-breuer@outlook.com>
2020-10-14 14:06:32 +0200

    fix syntax

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         'requests',
         'path<13.2.0',
         'schema-salad>=7.0.20200811075006',
-        'trs-cli'>=0.3.1
+        'trs-cli>=0.3.1'
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
