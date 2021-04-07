5e6cac048014e34e2653be019b81354cc71f49ae
KerstenBreuer <kersten-breuer@outlook.com>
2021-04-07 19:18:23 +0000

    change example workflow

--- cwlab/web_app/static/import_wf.js
+++ cwlab/web_app/static/import_wf.js
@@ -607,8 +607,8 @@ class ImportCwlUrl extends React.Component{
                                 </td>
                                 <td>
                                     You can directly use a URL from github. However, please make sure to provide the URL to the raw file, e.g.:<br/>
-                                    <a href="https://raw.githubusercontent.com/CompEpigen/ATACseq_workflows/1.2.92/CWL/workflows/ATACseq.cwl">
-                                    https://raw.githubusercontent.com/CompEpigen/ATACseq_workflows/1.2.92/CWL/workflows/ATACseq.cwl
+                                    <a href="https://raw.githubusercontent.com/CompEpigen/ChIPseq_workflows/master/CWL/workflows/ACTseq.cwl">
+                                    https://raw.githubusercontent.com/CompEpigen/ChIPseq_workflows/master/CWL/workflows/ACTseq.cwl
                                     </a>
                                 </td>
                             </tr>
